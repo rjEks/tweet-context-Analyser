c76dc4fdecbc2f09e5f71522a4ea00ed6420de1b
rjeks <rjekstein@gmail.com>
2022-07-31 23:31:52 -0300

    inital changes

--- tweet.py
+++ tweet.py
@@ -9,10 +9,8 @@ def set_credentials(bearer_token,consumer_key,consumer_secret,access_token,
 #get data from vault
     return True
 
-
 def getClient(bearer_token,consumer_key,consumer_secret,access_token,
-            access_token_secret):
-    
+            access_token_secret):    
 
     client = tw.Client(bearer_token=bearer_token, 
                        consumer_key=consumer_key, 
@@ -21,27 +19,15 @@ def getClient(bearer_token,consumer_key,consumer_secret,access_token,
                        access_token_secret=access_token_secret )
     return client
 
-def create_headers(bearer_token):
-    headers = {"Authorization": "Bearer {}".format(bearer_token)}
-    return headers
-
-def create_url(keyword, start_date, end_date, max_results = 10):
+def searchTwitter(client, start_time, end_time, max_results=10, query="#bolsonaro lang:pt"):
     
-    search_url = "https://api.twitter.com/2/tweets/search/recent" 
-    
-    query_params = {'query': keyword,
-                    'start_time': start_date,
-                    'end_time': end_date,
-                    'max_results': max_results,                  
-                    'next_token': {}}
-    return (search_url, query_params)
-
-
-def connect_to_endpoint(url, headers, params, next_token = None):
-    
-    params['next_token'] = next_token 
-    response = requests.request("GET", url, headers = headers, params = params)
-    
-    if response.status_code != 200:
-        raise Exception(response.status_code, response.text)
-    return response.json()
+    tweets = client.search_recent_tweets(query=query,
+                                     start_time=start_time,
+                                     end_time=end_time,
+                                     tweet_fields = ["created_at", "text", "source"],
+                                     user_fields = ["name", "username", "location", "verified", "description"],
+                                     max_results = max_results,
+                                     expansions='author_id'
+                                     )
+    tweets_dict = [dict(tweets_no_format) for tweets_no_format in tweets.data]
+    return tweets_dict
