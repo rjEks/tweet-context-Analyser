e3d9ea0f007ceb694c041c842a2b9606afca83d6
rjeks <rjekstein@gmail.com>
2022-08-01 23:33:55 -0300

    add comprehend.py

--- comprehend.py
+++ comprehend.py
@@ -1,3 +1,26 @@
 import os
 import boto3
-from tweet import *
+
+
+def detectSentiment(client, Text, LanguageCode="pt"):
+    
+    sentiment = client.detect_sentiment(Text=Text, LanguageCode=LanguageCode)
+    response_sentiment = sentiment['Sentiment']
+    response_scores = sentiment['SentimentScore']
+    
+    return response_sentiment, response_scores
+
+def detectEntities(client, Text, LanguageCode="pt"):
+    
+    entities = client.detect_entities(Text = Text, LanguageCode = LanguageCode)
+    entities = entities['Entities']
+    
+    textEntities = [dict_item['Text'] for dict_item in entities] 
+    typeEntities = [dict_item['Type'] for dict_item in entities] 
+    
+    return textEntities, typeEntities
+   
+    
+    
+    
+    
