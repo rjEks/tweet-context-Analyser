26887c0a86090b8138be31102db8afbb49cc1fb2
rjeks <rjekstein@gmail.com>
2022-08-02 22:14:03 -0300

    adjust secrets-manager and cfn

---
+++ src/secrets_manager.py
@@ -0,0 +1,39 @@
+import json
+import boto3
+from base64 import b64decode
+from botocore.exceptions import ClientError
+
+
+def get_secret():
+
+    secret_name = "twitter-analyser"
+    region_name = "us-west-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            raise e
+    else:
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            secret = b64decode(get_secret_value_response['SecretBinary'])
+    return json.loads(secret)
