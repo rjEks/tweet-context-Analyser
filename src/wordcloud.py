dd6da8cf6cdd544db277886a883b7a32355b6a54
rjeks <rjekstein@gmail.com>
2022-08-17 21:45:41 -0300

    adjust workflow

--- src/wordcloud.py
+++ src/wordcloud.py
@@ -1,2 +1,15 @@
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
+
+def generate_wordcloud(tweets):
+    tweets_text = " ".join(tweet for tweet in tweets)
+
+    word_cloud = WordCloud(
+        width=3000,
+        height=2000,
+        random_state=1,
+        background_color="salmon",
+        colormap="Pastel1",
+        collocations=False,
+        stopwords=STOPWORDS,
+        ).generate(tweets_text)
